877e402ff6a59542c04148c3b0184ec8916ae567
oolong <oolong@oolongs-MacBook-Air.local>
2023-04-14 11:28:55 -0700

    test changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Tyson Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Tyson Chae`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
